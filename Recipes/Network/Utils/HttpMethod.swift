f8d29991c907f522ba3afa8a3a36e9949dc8cf0b
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-03 22:24:08 +0700

    [Feature] Create Network Service protocol and Default Network Service protocol implementation including utils and errors

---
+++ Recipes/Network/Utils/HttpMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethod.swift
+//  Recipes
+//
+//  Created by Dylan on 3/7/25.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case post = "POST"
+    case get = "GET"
+    case put = "PUT"
+    case delete = "DELETE"
+}
